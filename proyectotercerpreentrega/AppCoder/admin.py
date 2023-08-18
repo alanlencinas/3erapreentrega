d82e4ff101a1fac999f87a5fbabd0ccb6a5905d6
Alan Gabriel Lencinas <alan.lencinas@gmail.com>
2023-08-17 23:57:53 -0300

    Avance2

--- proyectotercerpreentrega/AppCoder/admin.py
+++ proyectotercerpreentrega/AppCoder/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Perfumeria, Cabello, Clientes, Maquillaje, Cuidadocorporal
 
 # Register your models here.
+admin.site.register(Perfumeria)
+admin.site.register(Clientes)
+admin.site.register(Maquillaje)
+admin.site.register(Cabello)
+admin.site.register(Cuidadocorporal)
